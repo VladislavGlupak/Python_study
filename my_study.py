e6698a46ce7e16f2f4e4eabda372ff783cafa0db
Vladislav Glupak <vglupak@gmail.com>
2019-12-03 22:44:22 -0800

    zadanie

--- my_study.py
+++ my_study.py
@@ -1,9 +1,16 @@
-a = int(input())
-def check(a):
-    if -15<a<=12 or 14<a<17 or 19<=a:
-        return True
-    else:
-        return False
-print(check(a))
+#Task attributes
 
-print(5 /2 )
+class BlogPost:
+    def __init__(self, user_name, text, number_of_likes):
+        self.user_name = user_name
+        self.text = text
+        self.number_of_likes = number_of_likes
+
+post1 = BlogPost(user_name="Maks",text="Post number 1",number_of_likes=10)
+post2 = BlogPost(user_name="Alex",text="Post number 2",number_of_likes=20)
+
+post1.number_of_likes = 30
+
+print(post1.number_of_likes)
+print(post2.number_of_likes)
+
